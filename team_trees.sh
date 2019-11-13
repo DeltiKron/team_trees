88f492e0816c821efba51632a0d4737002847d6f
Carl Schaffer <cfmschaffer@hotmail.de>
2019-11-13 14:57:36 +0100

    Switching to relative paths

--- team_trees.sh
+++ team_trees.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 source ~/anaconda3/bin/activate
 conda activate sdc
-python /dat/schaffer/scripts/team_trees.py
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+python $DIR/team_trees.py
